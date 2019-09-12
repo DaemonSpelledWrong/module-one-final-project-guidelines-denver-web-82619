0f310606da508587070d2229e1edd6afa8ee8277
Damon Self <damongself@gmail.com>
2019-09-12 16:00:24 -0600

    Seeds database with API data. Updates class methods and Cli methods to accomodate.

--- lib/attack.rb
+++ lib/attack.rb
@@ -1,6 +1,21 @@
 # frozen_string_literal: true
 
+# class Attack contains methods to create attack instances
 class Attack < ActiveRecord::Base
   has_many :pokeatts
   has_many :pokemon, through: :pokeatts
+
+  def self.attack_names
+    response = RestClient.get('https://pokeapi.co/api/v2/move')
+    parsed_response = JSON.parse(response)
+    parsed_response['results'].map { |result| result['name'] }
+  end
+
+  def self.create_attacks
+    i = 0
+    while i < attack_names.length
+      create(name: attack_names[i], damage: rand(10..30))
+      i += 1
+    end
+  end
 end
--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# class Pokemon contains methods to interact with and create Pokemon instances
 class Pokemon < ActiveRecord::Base
   has_many :pokeatts
   has_many :attacks, through: :pokeatts
@@ -19,6 +20,20 @@ class Pokemon < ActiveRecord::Base
     end
     value.map do |pokeatt|
       pokeatt.attack.name
+    end
+  end
+
+  def self.pokemon_names
+    response = RestClient.get('https://pokeapi.co/api/v2/pokemon')
+    parsed_response = JSON.parse(response)
+    parsed_response['results'].map { |result| result['name'] }
+  end
+
+  def self.create_pokemons
+    i = 0
+    while i < pokemon_names.length
+      create(name: pokemon_names[i], health: rand(100..120))
+      i += 1
     end
   end
 end
